8d4a4e7c4577c7b8fa0e7c34c352d87920dce017
suharyadi.13 <suharyadi.13@gmail.com>
2022-06-06 19:14:36 +0700

    add db
    Signed-off-by: suharyadi.13 <suharyadi.13@gmail.com>

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "4dm1n",
-  DB: "assessment",
+  HOST: "ec2-34-225-159-178.compute-1.amazonaws.com",
+  USER: "xzbygkfofxithv",
+  PASSWORD: "4a87eb4c8e85ad490126193761e65f41ada86b4f4fcbb28975a53efd43dad6a2",
+  DB: "del7i9lolh5pkf",
   dialect: "postgres",
   pool: {
     max: 5,
