cb642925013ab273ab6639c7c01dc9660e1d7238
suharyadi.13 <suharyadi.13@gmail.com>
2022-06-06 18:19:37 +0700

    First Commit
    Signed-off-by: suharyadi.13 <suharyadi.13@gmail.com>

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "ec2-34-225-159-178.compute-1.amazonaws.com",
+  USER: "wweaeslrvkcdyl",
+  PASSWORD: "fba1e93959abbf1d905c551b9d9c5114f72df5ba29f3c69fc61ab7342325776f",
+  DB: "dbnpi33laerbk0",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
