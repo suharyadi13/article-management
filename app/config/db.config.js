6fba81ba2d9c29fb8c3601ff7e2b6378f5dd8710
suharyadi.13 <suharyadi.13@gmail.com>
2022-06-06 19:28:34 +0700

    update db
    Signed-off-by: suharyadi.13 <suharyadi.13@gmail.com>

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,7 @@ module.exports = {
   PASSWORD: "4a87eb4c8e85ad490126193761e65f41ada86b4f4fcbb28975a53efd43dad6a2",
   DB: "del7i9lolh5pkf",
   dialect: "postgres",
+  ssl: true,
   pool: {
     max: 5,
     min: 0,
