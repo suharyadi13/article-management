fc70f2cbd93c189d49f8973d7557cbfc834cf25b
suharyadi.13 <suharyadi.13@gmail.com>
2022-06-06 18:26:18 +0700

    update db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "ec2-34-225-159-178.compute-1.amazonaws.com",
-  USER: "wweaeslrvkcdyl",
-  PASSWORD: "fba1e93959abbf1d905c551b9d9c5114f72df5ba29f3c69fc61ab7342325776f",
-  DB: "dbnpi33laerbk0",
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "4dm1n",
+  DB: "assessment",
   dialect: "postgres",
   pool: {
     max: 5,
